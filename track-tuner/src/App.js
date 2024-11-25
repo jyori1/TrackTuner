132463aefad35c1de6f770b1cda2aeec479fa6b3
Simerjeet Mudhar <simerkm@gmail.com>
2024-11-25 13:18:53 -0500

    styling + basic routes

--- track-tuner/src/App.js
+++ track-tuner/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login.js';
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
--- track-tuner/src/components/Login.js
+++ track-tuner/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Spotify from '../Spotify.js'
+import './Login.css'
 
 const Login = () => {
     const signup = () => {
@@ -8,9 +9,14 @@ const Login = () => {
         })
     }
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <button className="btn" onClick={() => signup()}>Sign Up!</button>
+        <div className="container">
+            <div className="signup">
+                <h1>Sign Up with Spotify</h1>
+                <p>Discover your personalized playlists and more!</p>
+                <button className="btn" onClick={signup}>
+                    Sign Up with Spotify
+                </button>
+            </div>
         </div>
     )
 }
