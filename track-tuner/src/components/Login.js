19573d5380d611d127d1cfda1170316148f3a01d
Simerjeet Mudhar <simerkm@gmail.com>
2024-11-25 13:55:54 -0500

    basic nav after login -- small bug!!

--- track-tuner/src/components/Login.js
+++ track-tuner/src/components/Login.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Spotify from '../Spotify.js'
 import './Login.css'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    let navigate = useNavigate()
+    let [userData, setUserData] = useState("")
+
+    useEffect(() => {
+        if (userData){
+            navigate("/home")
+        }
+    })
+
     const signup = async () => {
         try {
             const newUserData = await Spotify.getUserId();
-            console.log(newUserData);
-          } catch (error) {
+            setUserData(newUserData)
+        } catch (error) {
             console.error('Error fetching user data:', error);
-          }
+        }
     }
     return (
         <div className="container">
             <div className="signup">
                 <h1>Sign Up with Spotify</h1>
                 <p>Discover your personalized playlists and more!</p>
-                <button className="btn" onClick={signup}>
+                <button className="btn" type="button" onClick={signup}>
                     Sign Up with Spotify
                 </button>
             </div>
